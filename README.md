7b1f2a45dead9d13e3ca9448ec635831e6dfb7e4
Nguyễn Hữu Giáp <giapnh@bap.jp>
2018-12-06 14:34:37 +0700

    Refactor code

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # extension-report-auto
-extensions chrome report template
-- Tạo template report chatwork
-- Tự động report sau 7h
-- Tìm id room chatwork để report hoặc id room default
-- Copy Clipboard
-- Edit report chatwork
+extensions chrome report auto
+- Auto report after 17h10 and before 8h
--- popup.js
+++ popup.js
@@ -31,7 +31,7 @@ function runscript(){
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
-    if(hour == 7 && 59 >= minute >= 20 ){
+    if((hour == 7 && 59 >= minute >= 20) || (hour == 17 && 10 <= minute <= 35)){
         chrome.tabs.executeScript(null, { file: "jquery.min.js" }, function() {
             chrome.tabs.executeScript(null, { code: "$('.claim-button').click()" });
         });
